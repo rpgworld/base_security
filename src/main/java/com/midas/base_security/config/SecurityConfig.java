ee7ed9f8ec896f0ca60d2f9baff190d0d9e668c3
rpgworld <cloud_data@naver.com>
2021-08-28 12:22:41 +0900

    Add Logout & Remember-Me

--- src/main/java/com/midas/base_security/config/SecurityConfig.java
+++ src/main/java/com/midas/base_security/config/SecurityConfig.java
@@ -6,12 +6,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -23,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
+        //== 폼 로그인 ==//
         http
                 .formLogin()
                 //.loginPage("/loginPage")                // 사용자 정의 로그인 페이지
@@ -48,6 +55,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .permitAll(); // loginPage 는 인가를 받지 않아도 접근 가능 하도록!
-        ;
+
+            //== 로그아웃 ==//
+            http
+                    .logout()   // post 방식으로 해야됨!
+                    .logoutUrl("/logout")                           // 로그아웃 처리 url
+                    .logoutSuccessUrl("/login")                     // 로그아웃 성공 후 이동페이지
+                    .deleteCookies("remember-me")                   // 로그아웃 후 쿠키 삭제
+                    // 로그아웃 핸들러
+                    .addLogoutHandler(new LogoutHandler() {
+                        @Override
+                        public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                            HttpSession session = request.getSession();
+                            session.invalidate();
+                        }
+                    })
+                    // 로그아웃 성공 후 핸들러
+                    .logoutSuccessHandler(new LogoutSuccessHandler() {
+                        @Override
+                        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            response.sendRedirect("/login");
+                        }
+                    });
+
+            //== Remember me ==//
+            http
+                    .rememberMe()
+                    .rememberMeParameter("remember")    // 기본 파라미터명: remember-me
+                    .tokenValiditySeconds(3600)         // 만료일 : Default 14 day
+                    .alwaysRemember(true)               // 활성화여부 : always
+                    .userDetailsService(userDetailsService());
     }
 }
